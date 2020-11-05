c2a978fffb51046e3435ee99f0249c07f0ebd543
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-11-06 00:54:22 +0200

    Add files via upload

---
+++ hopew4.py
@@ -0,0 +1,31 @@
+
+
+h=0
+s=0
+n=int(input("dati numarul: "))
+z=0
+while z<n:
+    z+=1
+    s=z+h
+    h=s
+
+
+print(s)
+h=1
+s=0
+z=0
+while z<n:
+    z+=1
+    s=z*h
+    h=s
+print(s)
+h=0
+s=0
+z=0
+while z<n:
+    z+=1
+    s=z+h
+    h=s
+print(s/n)
+
+    
